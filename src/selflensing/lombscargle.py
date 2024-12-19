314cbb0cfa094223ef511a4b2db766a421665a5e
Adam McMaster <me@adam-mcmaster.com>
2024-12-19 15:10:57 +0000

    Fix Lomb Scargle limits

--- src/selflensing/lombscargle.py
+++ src/selflensing/lombscargle.py
@@ -5,7 +5,7 @@ from astropy.timeseries import LombScargle
 
 def get_top_period(rv, times):
     maximum_frequency = 0.5
-    minimum_frequency = 1.0 / max(times)
+    minimum_frequency = 1.0 / max(times[-1] - times[0])
     frequency, power = LombScargle(times, rv).autopower(
         maximum_frequency=maximum_frequency, minimum_frequency=minimum_frequency
     )
